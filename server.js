1ab6a2b2c950edd421bf18a3233a348c2752137c
ymsebast <ymsebast@uci.edu>
2019-01-31 19:40:07 -0800

    added server.js

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var fs = require("fs");
 var PORT = 8080;
 
 var server = http.createServer(handleRequest);
-
+var firebase = require("firebase");
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCBdrmu0Zh7QJ4dl4IV9HCZNCH4y7QP9Lw",
+    authDomain: "test-43928.firebaseapp.com",
+    databaseURL: "https://test-43928.firebaseio.com",
+    projectId: "test-43928",
+    storageBucket: "test-43928.appspot.com",
+    messagingSenderId: "668914477501"
+};
+firebase.initializeApp(config);
 // Start our server
 server.listen(PORT, function () {
     // Callback triggered when server is successfully listening. Hurray!
@@ -35,11 +45,17 @@ function displayJSON(url, req, res) {
             throw err;
         }
         myHTML = JSON.parse(data);
+        var database = firebase.database();
         console.log(myHTML);
+        database.ref().set({
+            food: myHTML.responses[0].labelAnnotations[1].description,
+        });
         // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
         res.writeHead(200, { "Content-Type": "text/html" });
 
         // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
-        res.end(myHTML.responses[1].labelAnnotations[1].description);
+        res.end(myHTML.responses[0].labelAnnotations[1].description);
+        
+
     });
 }
