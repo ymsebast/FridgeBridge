07a99c7b01b9dd175f9ee390182c9714bacafd74
Eliza Ordonez <eordonez@uci.edu>
2019-02-09 03:26:14 +0000

    edited example.py

--- example.py
+++ example.py
@@ -3,8 +3,6 @@ import time
 import sys
 from hx711 import HX711
 
-
-
 def cleanAndExit():
     print "Cleaning..."
     GPIO.cleanup()
@@ -12,6 +10,7 @@ def cleanAndExit():
     sys.exit()
 
 hx = HX711(5, 6)
+refval = 100
 
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
@@ -20,15 +19,15 @@ hx = HX711(5, 6)
 # The first parameter is the order in which the bytes are used to build the "long" value.
 # The second paramter is the order of the bits inside each byte.
 # According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("MSB", "MSB")
+hx.set_reading_format("LSB", "MSB")
 
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
-hx.set_reference_unit(1)
+#hx.set_reference_unit(10)
+hx.set_reference_unit(refval)
 
 hx.reset()
 #hx.tare()
@@ -47,9 +46,11 @@ while True:
         #print binary_string + " " + np_arr8_string
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        # val = hx.get_weight(5)
-	val = hx.read_long()
+        val = max(0, int(hx.get_weight(5)))
+	#val = hx.read_long()
         print val
+	tval = val/refval 
+	print "True value in grams is: %d" % (tval)
 
         # To get weight from both channels (if you have load cells hooked up 
         # to both channel A and B), do something like this
