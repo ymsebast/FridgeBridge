e8093b0c9ead7710a8334ff5665f5b308de1a405
fellingt <fellingt@uci.edu>
2019-02-28 22:27:11 -0800

    Updated App

--- simpleList/app/src/main/java/com/example/simplelist/FoodList.java
+++ simpleList/app/src/main/java/com/example/simplelist/FoodList.java
@@ -8,9 +8,10 @@ import android.widget.TextView;
 public class FoodList extends AppCompatActivity {
 
     TextView tFood;
-    TextView tValue;
-    TextView tUnit;
-    TextView tGm;
+    TextView tCarbs;
+    TextView tFats;
+    TextView tKcal;
+    TextView tProtein;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +19,23 @@ public class FoodList extends AppCompatActivity {
         setContentView(R.layout.activity_food_list);
 
         tFood = (TextView) findViewById(R.id.textFood);
-        tValue = (TextView) findViewById(R.id.textValue);
-        tUnit = (TextView) findViewById(R.id.textUnit);
-        tGm = (TextView) findViewById(R.id.textGm);
+        tKcal = (TextView) findViewById(R.id.textKcal);
+        tCarbs = (TextView) findViewById(R.id.textCarbs);
+        tFats = (TextView) findViewById(R.id.textFats);
+        tProtein = (TextView) findViewById(R.id.textProtein);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String food_string = extras.getString("EXTRA_FOOD");
-        String value_string = extras.getString("EXTRA_VALUE");
-        String unit_string = extras.getString("EXTRA_UNIT");
-        String gm_string = extras.getString("EXTRA_GM");
+        String carbs_string = extras.getString("EXTRA_CARBS");
+        String fats_string = extras.getString("EXTRA_FATS");
+        String kcal_string = extras.getString("EXTRA_KCAL");
+        String protein_string = extras.getString("EXTRA_PROTEIN");
 
         tFood.setText(food_string);
-        tValue.setText(value_string);
-        tUnit.setText(unit_string);
-        tGm.setText(gm_string);
+        tKcal.setText(kcal_string);
+        tCarbs.setText(carbs_string);
+        tFats.setText(fats_string);
+        tProtein.setText(protein_string);
     }
 }
